c9965631eef3fdc66ceb7971d6cf5cd78e24cf03
Grupo3 - Alberto Carrillo Ortega <aco00032@red.ujaen.es>
2017-11-18 22:56:18 +0100

    Algoritmos geneticos Hotfix

--- AlgoritmosGeneticos.cpp
+++ AlgoritmosGeneticos.cpp
@@ -5,37 +5,37 @@
  */
 
 #include "AlgoritmosGeneticos.hpp"
-
-int geneticoGeneracional(int nIndividuos,int evaluaciones,int generaciones,int cruce,int tipo,int mutacion){
-    
-    Poblacion entorno(nIndividuos);
-    
-    while(evaluaciones >= 0){
-        
-        entorno.cruceGeneracional(cruce,tipo);
-        entorno.mutar(mutacion);
-        
-        if(entorno.evaluar==1)
-            entorno.reinicializar();
-        
-        evaluaciones-=nIndividuos*cruce;        
-    }
-    return entorno.getMejor().totalInterference_;
-}
-
-int geneticoEstacionario(int nIndividuos,int evaluaciones,int generaciones,int cruce,int tipo,int mutacion){
-    
-    Poblacion entorno(nIndividuos);
-    
-    while(evaluaciones >= 0){
-        
-        entorno.cruceEstacionario(cruce,tipo);
-        entorno.mutar(mutacion);
-        
-        if(entorno.evaluar==1)
-            entorno.reinicializar();
-        
-        evaluaciones-=2;        
-    }
-    return entorno.getMejor().totalInterference_;
-}
+//
+//int geneticoGeneracional(int nIndividuos,int evaluaciones,Poblacion& entorno,int cruce,int tipo,int mutacion){
+//    
+//
+//    
+//    while(evaluaciones >= 0){
+//        
+//        entorno.cruzeGeneracional(cruce,tipo);
+//        entorno.mutar(mutacion);
+//        
+//        if(entorno.evaluar==1)
+//            entorno.reinicializar();
+//        
+//        evaluaciones-=nIndividuos*cruce;        
+//    }
+//    return entorno.getMejor().totalInterference_;
+//}
+//
+//int geneticoEstacionario(int nIndividuos,int evaluaciones,Poblacion& entornos,int cruce,int tipo,int mutacion){
+//    
+//
+//    
+//    while(evaluaciones >= 0){
+//        
+//        entorno.cruzeEstacionario(cruce,tipo);
+//        entorno.mutar(mutacion);
+//        
+//        if(entorno.evaluar==1)
+//            entorno.reinicializar();
+//        
+//        evaluaciones-=2;        
+//    }
+//    return entorno.getMejor().totalInterference_;
+//}
--- AlgoritmosGeneticos.hpp
+++ AlgoritmosGeneticos.hpp
@@ -21,17 +21,26 @@ enum Cruce {
 /**
  * Algoritmo genético generacional con elitismo
  * @param nIndividuos       Numero de individuos de la poblacion con la que ejecutar el algoritmo
- * @param evaluaciones      Numero de evaluaciones de los individuos
- * @param generaciones      Numero de generaciones maximas //no en uso
+ * @param evaluciones       Numero de evaluaciones de los individuos
+ * @param entorno           Poblacion sobre la que aplicar el algoritmo
  * @param cruce             Probabilidad de Cruce
  * @param tipo              Tipo de cruce, 0 = BLX 1 = 2 Puntos
  * @param mutacion          Probabilidad de mutación
  * @return 
  */
-int geneticoGeneracional(int nIndividuos,int evaluaciones,int generaciones,int cruce,int tipo,int mutacion);
+int geneticoGeneracional(int nIndividuos,int evaluaciones,Poblacion& entorno,int cruce,int tipo,int mutacion);
 
-
-int geneticoEstacionario(int nIndividuos,int evaluaciones,int generaciones,int cruce,int tipo,int mutacion);
+/**
+ * Algoritmo genético estacionario
+ * @param nIndividuos       Numero de individuos de la poblacion con la que ejecutar el algoritmo
+ * @param evaluaciones      Numero de evaluaciones de los individuos
+ * @param entorno           Poblacion sobre la que aplicar el algoritmo
+ * @param cruce             Probabilidad de Cruce
+ * @param tipo              Tipo de cruce, 0 = BLX 1 = 2 Puntos
+ * @param mutacion          Probabilidad de mutación
+ * @return 
+ */
+int geneticoEstacionario(int nIndividuos,int evaluaciones,Poblacion& entorno,int cruce,int tipo,int mutacion);
 
 #endif /* ALGORITMOSGENETICOS_HPP */
 
