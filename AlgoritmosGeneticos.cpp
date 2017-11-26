a2c1e23d001304d3cd590a39b03c053e96bf6e6a
Grupo3 - Alberto Carrillo Ortega <aco00032@red.ujaen.es>
2017-11-26 21:21:40 +0100

    Fix en GeneticoGeneracional. Reinicar tras 20 generaciones sin mejora

--- AlgoritmosGeneticos.cpp
+++ AlgoritmosGeneticos.cpp
@@ -4,12 +4,27 @@
 int geneticoGeneracional(int nIndividuos,int evaluaciones,Poblacion& entorno,double cruce,int tipo,double mutacion){
     
     entorno.iniciarPoblacion(nIndividuos);
+    int generaciones= 20;
+    auto guardia = entorno.getMejor();
     while(evaluaciones >= 0){
         
         entorno.evolucionGeneracional(cruce,tipo,mutacion);
         
+        if(guardia != entorno.getMejor()){
+            generaciones = 20;
+            guardia=entorno.getMejor();
+        }else{
+            generaciones--;
+            if(generaciones==0){
+                entorno.reinicializar();
+                evaluaciones-=nIndividuos;
+                continue;
+            }
+        }
         if(entorno.comprobarRepetidos()){
             entorno.reinicializar();
+            evaluaciones-=nIndividuos-1;
+            continue;
         }
         
         evaluaciones-=nIndividuos*cruce;        
@@ -29,6 +44,8 @@ int geneticoEstacionario(int nIndividuos,int evaluaciones,Poblacion& entorno,int
         
         if(entorno.comprobarRepetidos()){
             entorno.reinicializar();
+            evaluaciones-=nIndividuos-1;
+            continue;
         }
         
         evaluaciones-=parejas*2;        
